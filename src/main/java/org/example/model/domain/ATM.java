5f87a252139519a903c406ea9e9061091c497395
Juanite <juan13_barsa@hotmail.com>
2024-03-06 08:57:18 +0100

    Model domain created

--- src/main/java/org/example/model/domain/ATM.java
+++ src/main/java/org/example/model/domain/ATM.java
@@ -1,4 +1,62 @@
 package org.example.model.domain;
 
+import java.util.Objects;
+
 public class ATM {
+    private int id;
+    private String name;
+    private boolean isInUse;
+
+    public ATM() {
+        this.id = -1;
+        this.name = "";
+        this.isInUse = false;
+    }
+    public ATM(String name) {
+        this.id = -1;
+        this.name = name;
+        this.isInUse = false;
+    }
+    public ATM(int id,String name, boolean isInUse) {
+        this.id = id;
+        this.name = name;
+        this.isInUse = isInUse;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isInUse() {
+        return isInUse;
+    }
+
+    public void setInUse(boolean inUse) {
+        isInUse = inUse;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ATM atm = (ATM) o;
+        return Objects.equals(name, atm.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
--- src/main/java/org/example/model/domain/User.java
+++ src/main/java/org/example/model/domain/User.java
@@ -1,4 +1,74 @@
 package org.example.model.domain;
 
+import java.util.Objects;
+
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private double money;
+
+    public User() {
+        this.id = -1;
+        this.username = "";
+        this.password = "";
+        this.money = 0.0;
+    }
+    public User(String username, String password, double money) {
+        this.id = -1;
+        this.username = username;
+        this.password = password;
+        this.money = money;
+    }
+    public User(int id, String username, String password, double money) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.money = money;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
 }
